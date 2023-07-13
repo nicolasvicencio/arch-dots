0ee7951f9da340f563e816621384ca63c129c5cc
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-07-12 21:58:31 -0400

    .

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #/bin/bash 
-yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh pamac-all steam github-cli  
+yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh pamac-all steam gh
