e2f5bb7d49a7deecee07acbda9f9d7a543bd7b14
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-07-25 16:59:17 -0400

    adding some app and walls

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
 #/bin/bash 
-yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh pamac-all steam gh
+yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh pamac-all steam github-cli latte-dock discord
+
+
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
