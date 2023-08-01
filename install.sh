58b53b7ede28fbeb2263b744d9db762158b219c9
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-08-01 18:03:21 -0400

    adding htop to packages

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash 
-yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli latte-dock discord p7zip onlyoffice-bin 
+yay -S base-devel anki visual-studio-code-bin android-studio vivaldi redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli latte-dock discord p7zip onlyoffice-bin htop 
 
 
 
