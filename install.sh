3b3e642f2ac9afc181537a7108d86dde43a06230
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-08-21 20:12:55 -0400

    adding hyprlnd

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash 
-yay -S base-devel anki visual-studio-code-bin android-studio redshift kitty lsd bat neovim noto-fonts-emoji mpv albert python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli latte-dock discord p7zip onlyoffice-bin htop octopi kvantum latte-dock ttf-jetbrains-mono-nerd cava kcolorchooser kdeplasma-addons
+yay -S base-devel anki visual-studio-code-bin  redshift  lsd bat  noto-fonts-emoji mpv  python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli  discord p7zip onlyoffice-bin htop  kvantum  ttf-jetbrains-mono-nerd cava kcolorchooser kdeplasma-addons
 
 
 
