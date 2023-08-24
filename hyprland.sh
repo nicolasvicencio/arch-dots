22f551dbf45456e319018149066546c3857cb08e
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-08-24 12:55:16 -0400

    adding hyprland script

---
+++ hyprland.sh
@@ -0,0 +1,1 @@
+yay -S waybar hyprland-git rofi dunst kitty swaybg swaylock-fancy-git swayidle pamixer light brillo
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/bin/bash 
-yay -S base-devel anki visual-studio-code-bin redshift  lsd bat  noto-fonts-emoji mpv  python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli  discord p7zip onlyoffice-bin htop  kvantum  ttf-jetbrains-mono-nerd cava kcolorchooser kdeplasma-addons graphicsmagick gs lf wl-clipboard
+yay -S base-devel anki neovim redshift  lsd bat  noto-fonts-emoji mpv  python-pip translate-shell ripgrep bitwarden lazygit npm pnpm zsh steam github-cli  discord p7zip onlyoffice-bin htop  kvantum  ttf-jetbrains-mono-nerd cava kcolorchooser kdeplasma-addons graphicsmagick gs lf wl-clipboard wine ttf-font-awesome
 
 
 
