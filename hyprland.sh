9f9fb171bddee19882dbdab96f3512919095752d
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-09-06 13:19:17 -0300

    adding screenshots pkgs

--- hyprland.sh
+++ hyprland.sh
@@ -1,1 +1,1 @@
-yay -S waybar-hyprland hyprland-git rofi dunst kitty swaybg swaylock-effects swayidle pamixer light brillo wlogout qt5ct swww-git wofi
+yay -S waybar-hyprland hyprland-git rofi dunst kitty swaybg swaylock-effects swayidle pamixer light brillo wlogout qt5ct swww-git wofi slurp swappy grim
