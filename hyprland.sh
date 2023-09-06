c95d35e417cca2af6a887486e1a6ccc44e34c9ea
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-09-05 23:58:37 -0300

    adding some walls and changer script

--- hyprland.sh
+++ hyprland.sh
@@ -1,1 +1,1 @@
-yay -S waybar-hyprland hyprland-git rofi dunst kitty swaybg swaylock-effects swayidle pamixer light brillo wlogout qt5ct
+yay -S waybar-hyprland hyprland-git rofi dunst kitty swaybg swaylock-effects swayidle pamixer light brillo wlogout qt5ct swww-git wofi
