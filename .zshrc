1d8c8d52e58dd4dadf696f43689647098c5c0670
Nicolas Vicencio <nicolas.vicencio.or@gmail.com>
2023-12-30 14:13:59 -0300

    adding a cursor and changing zsh config

--- .zshrc
+++ .zshrc
@@ -20,6 +20,7 @@ alias view='kitty +kitten icat'
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
+export CLICOLOR=1
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
--- install.sh
+++ install.sh
@@ -13,6 +13,27 @@ ___  ___       ______      _    __ _ _
 EOF
 echo -e "${NONE}"
 
+# ------------------------------------------------------
+# Check if yay is installed
+# ------------------------------------------------------
+echo -e "${NONE}"
+if sudo pacman -Qs yay > /dev/null ; then
+    echo "yay is already installed!"
+else
+    echo "yay is not installed. Will be installed now!"
+    _installPackagesPacman "base-devel"
+    SCRIPT=$(realpath "$0")
+    temp_path=$(dirname "$SCRIPT")
+    echo $temp_path
+    git clone https://aur.archlinux.org/yay-git.git ~/yay-git
+    cd ~/yay-git
+    makepkg -si
+    cd $temp_path
+    echo "yay has been installed successfully."
+fi
+echo ""
+
+
 #Update repos
 echo "Remember update your packages repositories"
 echo ""
